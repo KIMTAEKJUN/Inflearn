0c51ea7a1903bcdbf005887449f344215ee9ed39
KimTaekJun <86834898+Kuah0@users.noreply.github.com>
2022-01-25 19:05:16 +0900

    css 수정, js, html

--- VaillnaJS_Browser/js/greetings.js
+++ VaillnaJS_Browser/js/greetings.js
@@ -16,6 +16,7 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.style.display = "none";
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
